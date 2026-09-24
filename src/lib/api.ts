9224edfcfd29becee8672f22ddd66b3b79571e61
RDI-7 <rafiul07islam@gmail.com>
2026-09-24 16:20:00 +0600

    setup workout types and API integration

---
+++ src/lib/api.ts
@@ -0,0 +1,24 @@
+import { Workout } from "@/types/workout";
+
+const API_URL = "https://api.abcz.workers.dev/api/fitlog";
+
+export async function getWorkouts():Promise<Workout[]> {
+    const res = await fetch(API_URL)
+
+    if(!res.ok) {
+        throw new Error ("Failed to fetch workouts")
+    }
+
+    return res.json();
+}
+
+export async function getWorkout(id: string):Promise<Workout> {
+    const res = await fetch(`${API_URL}/${id}`);
+
+    if(!res.ok) {
+        throw new Error ("Workout not found")
+    }
+
+    return res.json();
+}
+
